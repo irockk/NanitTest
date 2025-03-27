827c3aa7932f3b113dab31e55a622b0242d1f712
irinakosenkova <Lrinakos2002@gmail.com>
2025-03-27 20:39:12 +0200

    Add colors for birthday screen.

--- app/src/main/java/com/example/nanit/ui/theme/Color.kt
+++ app/src/main/java/com/example/nanit/ui/theme/Color.kt
@@ -9,3 +9,12 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val BlueLight = Color(0xFFDAF1F6)
+val BlueDark = Color(0xFF8BD3E4)
+
+val GreenLight = Color(0xFFC5E8DF)
+val GreenDark = Color(0xFF6FC5AF)
+
+val YellowLight = Color(0xFFFEEFCB)
+val YellowDark = Color(0xFFFEBE21)
