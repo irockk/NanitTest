4864a78d14501d5ff770e19ba8d98b2105cfbf0c
irinakosenkova <Lrinakos2002@gmail.com>
2025-03-28 18:35:41 +0200

    Add share button with no logic.

--- app/src/main/java/com/example/nanit/feature/birthday/presentation/BirthdayScreen.kt
+++ app/src/main/java/com/example/nanit/feature/birthday/presentation/BirthdayScreen.kt
@@ -1,17 +1,24 @@
 package com.example.nanit.feature.birthday.presentation
 
 import android.net.Uri
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.zIndex
+import com.example.nanit.R
 import com.example.nanit.feature.birthday.presentation.components.BaseBirthdayScreen
 import com.example.nanit.feature.birthday.presentation.models.BirthdayTheme
 import com.example.nanit.feature.birthday.presentation.models.toStringRes
@@ -24,15 +31,26 @@ fun BirthdayScreen(
 ) {
     val randomScreenIndex = remember { (0..2).random() }
 
-    IconButton(
-        modifier = Modifier.zIndex(2f),
-        onClick = goBack
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .zIndex(2f),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
     ) {
-        Icon(
-            modifier = Modifier.fillMaxSize(),
-            painter = rememberVectorPainter(Icons.Filled.Clear),
-            contentDescription = null
-        )
+        IconButton(onClick = goBack) {
+            Icon(
+                modifier = Modifier.fillMaxSize(),
+                painter = rememberVectorPainter(Icons.Filled.Clear),
+                contentDescription = null
+            )
+        }
+
+        TextButton(onClick = {
+
+        }) {
+            Text(text = stringResource(R.string.birthday_share_button))
+        }
     }
 
     when (randomScreenIndex) {
